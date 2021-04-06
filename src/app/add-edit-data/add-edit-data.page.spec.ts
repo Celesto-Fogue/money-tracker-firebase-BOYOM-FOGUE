a8857e1a916bc3c79420f05e15c2529127fb6381
Celesto-Fogue <celestofogue@gmail.com>
2021-04-06 07:32:14 +0200

    1.0

--- src/app/add-edit-data/add-edit-data.page.spec.ts
+++ src/app/add-edit-data/add-edit-data.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { AddEditDataPage } from './add-edit-data.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('AddEditDataPage', () => {
+  let component: AddEditDataPage;
+  let fixture: ComponentFixture<AddEditDataPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ AddEditDataPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(AddEditDataPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
